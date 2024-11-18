59a624cac8b49331a7caa0aa2976b2dd1307faf4
JuliaLopesCoimbra <juliacristinalopes2607@gmail.com>
2024-11-18 08:10:11 -0300

    first

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './styles/App.css';
+import { useState } from 'react';
+import { makeRequest } from './api/api';
+import SideMenu from './components/SideMenu/SideMenu';
+function App() {
+  return (
+    <div className="App">
+      <SideMenu />
+      <h1>oi</h1>
+    </div>
+  );
+}
+
+export default App;
